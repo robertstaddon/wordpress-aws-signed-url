59d4f3d1210ff50e24c0224ff36ba6ff71c94a35
Robert Staddon <robert@abundantdesigns.com>
2022-04-21 10:29:55 -0500

    Change plugin to "AWS Get Signed CloudFront URL"

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# AWS Signed URL Plugin #
+# AWS Get Signed CloudFront URL #
 
-This plugin will generate an Signed URL to enable private content to be served through Amazon CloudFront. A CloudFront
+This plugin provides a function to generate a signed URL to enable private content to be served through Amazon CloudFront. A CloudFront
 key pair has to be created to configure the plugin and the CloudFront distribution configured appropriately. 
 Full details of this process can be found in the 
 [AWS documentation](http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/PrivateContent.html)
 
-This plugin does not manage the process of getting Media Assets on to Amazon S3 or modify the Domain name for AWS based
-media. In order to do that a plugin such as Delicious Brains' [WP Offload S3](https://wordpress.org/plugins/amazon-s3-and-cloudfront/)
-should be used.
+Use AWSSignedURL:get_signed_URL( $resource ) in your code to retrieve a signed AWS CloudFront URL, where $resource is the URL path
+to the resource for which you need a signed URL.
 
-
+This plugin was forked from the "AWS Signed URL Plugin" from Orca Studios: https://github.com/ocastastudios/wordpress-aws-signed-url
 
--- aws-signed-url.php
+++ aws-signed-url.php
@@ -1,24 +1,14 @@
 <?php
 
 /*
-Plugin Name: AWS Signed URLs
-Description: Generates signed urls for Cloudfront assets
-Version: 1.0.1
-Author: Ocasta Studios
+  Plugin Name: AWS Get Signed CloudFront URL
+  Description: Generates signed urls for Cloudfront assets
+  Version: 1.0.1
+  Author: Abundant Designs LLC
 
-Copyright 2016 Ocasta Studios Ltd
+  Use AWSSignedURL:get_signed_URL( $resource ) in your code to retrieve a signed AWS CloudFront URL, where $resource is the URL path
+  to the resource for which you need a signed URL.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 */
 
 
@@ -29,39 +19,35 @@ class AWSSignedURL
 {
 
   public function __construct() {
-    require_once(plugin_dir_path(__FILE__) . '/aws-signed-url-options.php');
+    require_once( plugin_dir_path(__FILE__) . '/aws-signed-url-options.php' );
     new AWSSignedURL_Options();
-
-    add_filter('wp_get_attachment_url', array($this,'get_signed_URL'),100);
   }
 
   // Create a Signed URL for media assets stored on S3 and served up via CloudFront
-  function get_signed_URL($resource) {
+  public function get_signed_URL( $resource ) {
     $options = get_option('aws_signed_url_settings');
 
-    $expires = time() + $options['aws_signed_url_lifetime']; //Time out in seconds
+    $expires = time() + $options['aws_signed_url_lifetime']; // Time out in seconds
     $json = '{"Statement":[{"Resource":"'.$resource.'","Condition":{"DateLessThan":{"AWS:EpochTime":'.$expires.'}}}]}';
 
-    //Read the private key
+    // Read the private key
     $key = openssl_get_privatekey($options['aws_signed_url_pem']);
-    if(!$key)
-    {
+    if ( !$key ) {
       error_log( 'Failed to read private key: '.openssl_error_string() );
       return $resource;
     }
 
-    //Sign the policy with the private key
-    if(!openssl_sign($json, $signed_policy, $key, OPENSSL_ALGO_SHA1))
-    {
+    // Sign the policy with the private key
+    if ( !openssl_sign( $json, $signed_policy, $key, OPENSSL_ALGO_SHA1 ) ) {
       error_log( 'Failed to sign url: '.openssl_error_string());
       return $resource;
     }
 
-    //Create signiature
-    $base64_signed_policy = base64_encode($signed_policy);
-    $signature = str_replace(array('+','=','/'), array('-','_','~'), $base64_signed_policy);
+    // Create signature
+    $base64_signed_policy = base64_encode( $signed_policy );
+    $signature = str_replace( array('+','=','/'), array('-','_','~'), $base64_signed_policy );
 
-    //Construct the URL
+    // Construct the URL
     $url = $resource.'?Expires='.$expires.'&Signature='.$signature.'&Key-Pair-Id='.$options['aws_signed_url_key_pair_id'];
 
     return $url;
